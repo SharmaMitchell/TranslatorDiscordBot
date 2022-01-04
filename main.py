ee91a3ca0da23d76e2903e4044914baa0e6df47f
Mitchell <90817905+SharmaMitchell@users.noreply.github.com>
2022-01-04 08:59:47 +0000

    Removed cogs (unused), added '!help' guide, and '!tl' shortcut

--- main.py
+++ main.py
@@ -12,11 +12,14 @@ bot.author_id = 157610726326927361
 
 @bot.event 
 async def on_ready():  # When the bot is ready
-    print("I'm in")
+    print("Bot connected!")
     print(bot.user)  # Prints the bot's username and ID
 
-@bot.command()
+@bot.command(name="translate", aliases=["tl"])
 async def translate(ctx,*args):
+	'''Translates JP -> EN, and vice-versa.\n
+	Reply to a message you wish to translate with: !translate\n
+	Or, enter: !translate [Sentence to be translated]'''
 	translateMe = ""
 	for arg in args: translateMe += arg + " "
 	if translateMe == "":
@@ -25,7 +28,6 @@ async def translate(ctx,*args):
 	if(translateMe.isascii()): # Text is EN if all chars are ascii
 		source_lang = 'EN'
 		target_lang = 'JA'
-		print("EN->JP")
 	else: # Else, assume text is JA (impetfect, but should cover most cases)
 		source_lang = 'JA'
 		target_lang = 'EN'
@@ -33,22 +35,12 @@ async def translate(ctx,*args):
 	headers = {'text' : translateMe, 'source_lang' : source_lang, 'target_lang' : target_lang}
 	response = requests.get(url, headers)
 	responseJSON = response.json()
-	print(responseJSON)
 	try:
 		result = responseJSON['translations'][0]['text']
 		await ctx.reply(result)
 	except:
 		errMsg = "Translation failed. Error code: {}".format(response.status_code)
 		await ctx.reply(errMsg)
-
-extensions = [ # Not using cogs for now
-	#'cogs.cog_example'
-	#'cogs.cog_translate'
-]
-
-if __name__ == '__main__':  # Ensures this is the file being ran
-	for extension in extensions:
-		bot.load_extension(extension)  # Loades every extension.
 
 keep_alive()  # Starts a webserver to be pinged.
 token = os.environ.get("DISCORD_BOT_SECRET") 
