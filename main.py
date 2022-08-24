abf73271ccd83ed31c6b4cbc018eb4a62f8c9bb3
Mitchell <90817905+SharmaMitchell@users.noreply.github.com>
2022-08-24 02:42:21 +0000

    Added CSV log file to track translation requests and errors

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import os
 import requests
 import emoji
+import csv
 from keep_alive import keep_alive
 from discord.ext import commands
+from datetime import datetime
 
 bot = commands.Bot(
 	command_prefix="!",  # Change to desired prefix
@@ -10,6 +12,14 @@ bot = commands.Bot(
 )
 
 bot.author_id = 157610726326927361
+
+def log(user, server, channel, source_lang, target_lang, translateMe, result):
+	with open('./log.csv', 'a', encoding='UTF8', newline='') as f:
+		now = datetime.now()
+		now = now.strftime("%Y-%m-%d %H:%M:%S")
+		row = [now, user, server, channel, source_lang, target_lang, translateMe, result]
+		writer = csv.writer(f)
+		writer.writerow(row)
 
 @bot.event 
 async def on_ready():  # When the bot is ready
@@ -21,30 +31,40 @@ async def translate(ctx,*args):
 	'''Translates JP -> EN, and vice-versa.\n
 	Reply to a message you wish to translate with: !translate\n
 	Or, enter: !translate [Sentence to be translated]'''
+
+	channel = ctx.channel.name
+	server = ctx.guild.name
+	user = ctx.author
+	
 	translateMe = ""
 	for arg in args: translateMe += arg + " "
-	print(translateMe)
+
+	#print('{} from {} server in {} channel sent {}'.format(user, server, channel, translateMe))
+	
+	print("Translation request: ", translateMe)
 	if translateMe == "": # If no args, get the msg this command replied to
 		translateMe = await ctx.channel.fetch_message(ctx.message.reference.message_id)
 		translateMe = translateMe.content
 	if(emoji.demojize(translateMe).isascii()): # Text is EN if all chars (excl emoji) are ascii
 		source_lang = 'EN'
 		target_lang = 'JA'
-		print(translateMe, "is English")
 	else: # Else, text is JA
 		source_lang = 'JA'
 		target_lang = 'EN'
-		print(translateMe, "is Japanese")
 	url = "https://api-free.deepl.com/v2/translate?auth_key={}".format(os.getenv('DEEPL_API_KEY'))
 	headers = {'text' : translateMe, 'source_lang' : source_lang, 'target_lang' : target_lang}
 	response = requests.get(url, headers)
 	responseJSON = response.json()
 	try:
 		result = responseJSON['translations'][0]['text']
+		print("Translated output: ", result)
 		await ctx.reply(result)
+		log(user, server, channel, source_lang, target_lang, translateMe, result)
 	except:
 		errMsg = "Translation failed. Error code: {}".format(response.status_code)
+		print("Error: ", errMsg)
 		await ctx.reply(errMsg)
+		log(user, server, channel, source_lang, target_lang, translateMe, errMsg)
 
 keep_alive()  # Starts a webserver to be pinged.
 token = os.environ['DISCORD_BOT_SECRET']
