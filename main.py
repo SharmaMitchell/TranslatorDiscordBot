424d3021fee5c693b9e4d8a50d0d6dc3c18881a9
Mitchell <90817905+SharmaMitchell@users.noreply.github.com>
2022-08-24 01:42:41 +0000

    Fixed issue: text containing emojis always treated as japanese Used emoji library to "demojize" text (turn emojis into ascii-compatible english) This allowed me to check if demojized strings were ascii Strings containing any non-ascii chars (excl emoji) are still considered japanese

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import requests
+import emoji
 from keep_alive import keep_alive
 from discord.ext import commands
 
@@ -22,15 +23,18 @@ async def translate(ctx,*args):
 	Or, enter: !translate [Sentence to be translated]'''
 	translateMe = ""
 	for arg in args: translateMe += arg + " "
-	if translateMe == "":
+	print(translateMe)
+	if translateMe == "": # If no args, get the msg this command replied to
 		translateMe = await ctx.channel.fetch_message(ctx.message.reference.message_id)
 		translateMe = translateMe.content
-	if(translateMe.isascii()): # Text is EN if all chars are ascii
+	if(emoji.demojize(translateMe).isascii()): # Text is EN if all chars (excl emoji) are ascii
 		source_lang = 'EN'
 		target_lang = 'JA'
-	else: # Else, assume text is JA (impetfect, but should cover most cases)
+		print(translateMe, "is English")
+	else: # Else, text is JA
 		source_lang = 'JA'
 		target_lang = 'EN'
+		print(translateMe, "is Japanese")
 	url = "https://api-free.deepl.com/v2/translate?auth_key={}".format(os.getenv('DEEPL_API_KEY'))
 	headers = {'text' : translateMe, 'source_lang' : source_lang, 'target_lang' : target_lang}
 	response = requests.get(url, headers)
@@ -43,5 +47,11 @@ async def translate(ctx,*args):
 		await ctx.reply(errMsg)
 
 keep_alive()  # Starts a webserver to be pinged.
-token = os.environ.get("DISCORD_BOT_SECRET") 
-bot.run(token)  # Starts the bot
+token = os.environ['DISCORD_BOT_SECRET']
+try:
+	bot.run(token)  # Starts the bot
+except Exception as e:
+	print("Bot unable to start")
+	print(e.__class__)
+	if bot.is_ws_ratelimited():
+		print("Rate Limited, Unable to start")
